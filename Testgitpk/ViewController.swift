0ae3a3dea4fa033fb6aa7eddbc939d5e19f4d45e
Sandeep Kumar <mobappssolutions132@gmail.com>
2022-07-25 11:45:40 +0530

    Testing....

--- Testgitpk/ViewController.swift
+++ Testgitpk/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+        
+        let namelbl = UILabel()
+        namelbl.frame = CGRect(x: 20, y: 150, width: view.frame.size.width-40, height: 80)
+        namelbl.text = "Jai Shri Ram ji"
+        namelbl.textColor = .white
+        namelbl.textAlignment = .center
+        namelbl.font = UIFont.boldSystemFont(ofSize: 24)
+        view.addSubview(namelbl)
+        
     }
 
 
