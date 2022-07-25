89abcbde32b5933ddbce1b62524d1aecc7472933
Sandeep Kumar <mobappssolutions132@gmail.com>
2022-07-25 11:53:45 +0530

    Some String text change

--- Testgitpk/ViewController.swift
+++ Testgitpk/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         
         let namelbl = UILabel()
         namelbl.frame = CGRect(x: 20, y: 150, width: view.frame.size.width-40, height: 80)
-        namelbl.text = "Hi sir Jai Shri Ram ji"
+        namelbl.text = "Hello sir Jai Shri Ram ji"
         namelbl.textColor = .white
         namelbl.textAlignment = .center
         namelbl.font = UIFont.boldSystemFont(ofSize: 24)
