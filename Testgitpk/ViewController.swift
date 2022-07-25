420091b3e91720a7569945fdfb9cec262f9eb728
Sandeep Kumar <mobappssolutions132@gmail.com>
2022-07-25 12:38:04 +0530

    hgfhjygfjhgjhk hjgjguj

--- Testgitpk/ViewController.swift
+++ Testgitpk/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         namelbl.text = "Narender sir Jai Shri Ram ji"
         namelbl.textColor = .white
         namelbl.textAlignment = .center
-        namelbl.font = UIFont.boldSystemFont(ofSize: 24)
+        namelb.font = UIFont.boldSystemFont(ofSize: 24)
         view.addSubview(namelbl)
         
         
